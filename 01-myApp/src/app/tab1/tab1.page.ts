682dbbb92aa4dd74edc36993719f55b995258569
michael rondon <mrondon72@gmail.com>
2019-05-11 23:57:41 +0200

    14. Depurar una aplicacion de ionic - debugger

--- 01-myApp/src/app/tab1/tab1.page.ts
+++ 01-myApp/src/app/tab1/tab1.page.ts
@@ -1,8 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page {}
+export class Tab1Page implements OnInit {
+
+  ngOnInit() {
+
+    let numero = 10;
+
+    debugger;
+
+    numero += 10;
+
+    numero *= 2;
+
+    numero -= 10;
+
+    console.log({ numero });
+
+  }
+
+}
