82059dac5bba25774bb8e2b68a9268f8bbfa702a
michael rondon <mrondon72@gmail.com>
2019-05-16 18:07:30 +0200

    24. Servicio y data externa

--- 02-angularBases/src/app/app.module.ts
+++ 02-angularBases/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 // Components
 import { AppComponent } from './app.component';
@@ -19,6 +20,7 @@ import { PagesModule } from './pages/pages.module';
   imports: [
     AppRoutingModule,
     BrowserModule,
+    HttpClientModule,
     PagesModule
   ],
   providers: [],
--- 02-angularBases/src/app/pages/posts/posts.component.ts
+++ 02-angularBases/src/app/pages/posts/posts.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DataService } from '../../services/data.service';
 
 @Component({
   selector: 'app-posts',
@@ -7,9 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PostsComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private dataService: DataService
+  ) { }
 
   ngOnInit() {
+
+    this.dataService.getPosts().subscribe( posts => {
+
+      console.log(posts);
+
+    });
+
   }
 
 }
