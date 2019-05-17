cc166ec4a163da1d5c821c2c2749252cf0d60076
michael rondon <mrondon72@gmail.com>
2019-05-18 00:22:36 +0200

    36. ion-list: Lista en ionic - Parte 1

--- 03-componentes/src/app/pages/inicio/inicio.page.ts
+++ 03-componentes/src/app/pages/inicio/inicio.page.ts
@@ -7,9 +7,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InicioPage implements OnInit {
 
+  componentes: Componente[] = [
+    {
+      icon: 'american-football',
+      name: 'Action Sheet',
+      redirectTo: '/action-sheet'
+    },
+    {
+      icon: 'appstore',
+      name: 'Alert',
+      redirectTo: '/alert'
+    }
+  ];
+
   constructor() { }
 
   ngOnInit() {
   }
 
+}
+
+interface Componente {
+  icon: string;
+  name: string;
+  redirectTo: string;
 }
