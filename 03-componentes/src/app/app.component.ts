4bfe367cbc8d0c959cecd74e5592f5c6508485d8
mrondon <mrondon-externo@cnmc.es>
2019-05-31 13:54:42 +0200

    73. ion-split-pane

--- 03-componentes/src/app/app.component.ts
+++ 03-componentes/src/app/app.component.ts
@@ -3,16 +3,23 @@ import { Component } from '@angular/core';
 import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { Observable } from 'rxjs';
+import { Componente } from './interfaces/interfaces';
+import { DataService } from './services/data.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html'
 })
 export class AppComponent {
+
+  componentes: Observable<Componente[]>;
+
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
-    private statusBar: StatusBar
+    private statusBar: StatusBar,
+    private dataService: DataService
   ) {
     this.initializeApp();
   }
@@ -21,6 +28,8 @@ export class AppComponent {
     this.platform.ready().then(() => {
       this.statusBar.styleDefault();
       this.splashScreen.hide();
+
+      this.componentes = this.dataService.getMenuOpts();
     });
   }
 }
