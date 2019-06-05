7b6bc69f612b3506ce51bedf4973e613b7e5205b
Michael Rondon <mrondon72@gmail.com>
2019-06-05 10:24:58 +0200

    104. Mostrar action-sheet en las noticias

--- 04-noticias/src/app/components/noticia/noticia.component.ts
+++ 04-noticias/src/app/components/noticia/noticia.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { InAppBrowser } from '@ionic-native/in-app-browser/ngx';
+import { ActionSheetController } from '@ionic/angular';
 
 // Interfaces
 import { Article } from '../../interfaces/interfaces';
@@ -15,7 +16,8 @@ export class NoticiaComponent implements OnInit {
   @Input() noticia: Article;
 
   constructor(
-    private iab: InAppBrowser
+    private iab: InAppBrowser,
+    private actionSheetCtrl: ActionSheetController
   ) { }
 
   ngOnInit() {}
@@ -23,6 +25,42 @@ export class NoticiaComponent implements OnInit {
   abrirNoticia() {
 
     const browser = this.iab.create(this.noticia.url, '_system');
+
+  }
+
+  async lanzarMenu() {
+
+    const actionSheet = await this.actionSheetCtrl.create({
+      buttons: [
+        {
+          text: 'Compartir',
+          icon: 'share',
+          cssClass: 'action-dark',
+          handler: () => {
+            console.log('Share clicked');
+          }
+        },
+        {
+          text: 'Favorito',
+          icon: 'star',
+          cssClass: 'action-dark',
+          handler: () => {
+            console.log('Favorito');
+          }
+        },
+        {
+          text: 'Cancelar',
+          icon: 'close',
+          cssClass: 'action-dark',
+          role: 'cancel',
+          handler: () => {
+            console.log('Cancel clicked');
+          }
+        }
+      ]
+    });
+
+    await actionSheet.present();
 
   }
 
