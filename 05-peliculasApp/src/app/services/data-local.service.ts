e2b58aae74b06fbb8aebc63b5b8bec863c4a5813
Michael Rondon <mrondon72@gmail.com>
2019-06-20 17:03:35 +0200

    143. Prevenir duplicados en nuestro storage de peliculas

--- 05-peliculasApp/src/app/services/data-local.service.ts
+++ 05-peliculasApp/src/app/services/data-local.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { PeliculaDetalle } from '../interfaces/interfaces';
 import { Storage } from '@ionic/storage';
+import { ToastController } from '@ionic/angular';
 
 @Injectable({
   providedIn: 'root'
@@ -10,14 +11,56 @@ export class DataLocalService {
   peliculas: PeliculaDetalle[] = [];
 
   constructor(
-    private storage: Storage
+    private storage: Storage,
+    private toastCtrl: ToastController
   ) { }
 
   guardarPelicula( pelicula: PeliculaDetalle ) {
 
-    this.peliculas.push( pelicula );
+    let existe = false;
+
+    let mensaje = '';
+
+    for ( const peli of this.peliculas ) {
+
+      if ( peli.id === pelicula.id ) {
+
+        existe = true;
+
+        break;
+
+      }
+
+    }
+
+    if ( existe ) {
+
+      this.peliculas = this.peliculas.filter( peli => peli.id !== pelicula.id );
+
+      mensaje = 'Removido de favoritos';
+
+    } else {
+
+      this.peliculas.push( pelicula );
+
+      mensaje = 'Agregada a favoritos';
+
+    }
+
+    this.presentToast( mensaje );
 
     this.storage.set('peliculas', this.peliculas);
+
+  }
+
+  async presentToast( message: string ) {
+
+    const toast = await this.toastCtrl.create({
+      message,
+      duration: 1500
+    });
+
+    toast.present();
 
   }
 }
