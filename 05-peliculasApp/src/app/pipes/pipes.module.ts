ea0c40191c74d8f0d5e2e791dfd3bc86fafca171
Michael Rondon <mrondon72@gmail.com>
2019-06-20 18:34:49 +0200

    147. Pequeños arreglos esteticos en ios

--- 05-peliculasApp/src/app/pipes/pipes.module.ts
+++ 05-peliculasApp/src/app/pipes/pipes.module.ts
@@ -1,14 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FiltroImagenPipe } from './filtro-imagen.pipe';
 import { ImagenPipe } from './imagen.pipe';
 import { ParesPipe } from './pares.pipe';
 
 @NgModule({
   declarations: [
+    FiltroImagenPipe,
     ImagenPipe,
     ParesPipe
   ],
   exports: [
+    FiltroImagenPipe,
     ImagenPipe,
     ParesPipe
   ],
