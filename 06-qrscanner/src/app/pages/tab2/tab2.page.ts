b3d37e107c217c021ccc9c89ed4d89809587305b
mrondon <mrondon-externo@cnmc.es>
2019-06-25 13:25:49 +0200

    158. Mostrar registrps en el historial

--- 06-qrscanner/src/app/pages/tab2/tab2.page.ts
+++ 06-qrscanner/src/app/pages/tab2/tab2.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { DataLocalService } from '../../services/data-local.service';
 
 @Component({
   selector: 'app-tab2',
@@ -7,6 +8,20 @@ import { Component } from '@angular/core';
 })
 export class Tab2Page {
 
-  constructor() {}
+  constructor(
+    public dataLocalService: DataLocalService
+  ) {}
+
+  enviarCorreo() {
+
+    console.log('enviando correo');
+
+  }
+
+  abrirRegistro( registro ) {
+
+    console.log('Registro', registro);
+
+  }
 
 }
