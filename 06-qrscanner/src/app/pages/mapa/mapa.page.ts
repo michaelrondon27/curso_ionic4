42e87c879d32ae5463bc7cbf2c34959301ba265b
mrondon <mrondon-externo@cnmc.es>
2019-06-25 16:26:43 +0200

    164. Mostrar el mapa con las coordenadas recibidas

--- 06-qrscanner/src/app/pages/mapa/mapa.page.ts
+++ 06-qrscanner/src/app/pages/mapa/mapa.page.ts
@@ -1,12 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+
+declare var mapboxgl: any;
 
 @Component({
   selector: 'app-mapa',
   templateUrl: './mapa.page.html',
   styleUrls: ['./mapa.page.scss'],
 })
-export class MapaPage implements OnInit {
+export class MapaPage implements OnInit, AfterViewInit {
 
   lat: number;
 
@@ -27,6 +29,16 @@ export class MapaPage implements OnInit {
     this.lng = Number(geo[1]);
 
     console.log(this.lat, this.lng);
+
+  }
+
+  ngAfterViewInit() {
+
+    mapboxgl.accessToken = 'pk.eyJ1IjoibXJvbmRvbjcyIiwiYSI6ImNqeGJyNnVhYzA3MWwzeXBlaHlzc3ZleG4ifQ.TwvVQCK3WtQFEVKGdEp1kg';
+    const map = new mapboxgl.Map({
+      container: 'map',
+      style: 'mapbox://styles/mapbox/streets-v11'
+    });
 
   }
 
