52b84bfce575ed4545b694e2fcf5b638c5b151ba
mrondon <mrondon-externo@cnmc.es>
2019-06-25 16:40:33 +0200

    165. Edificios 3D y marcadores

--- 06-qrscanner/src/app/pages/mapa/mapa.page.ts
+++ 06-qrscanner/src/app/pages/mapa/mapa.page.ts
@@ -35,9 +35,72 @@ export class MapaPage implements OnInit, AfterViewInit {
   ngAfterViewInit() {
 
     mapboxgl.accessToken = 'pk.eyJ1IjoibXJvbmRvbjcyIiwiYSI6ImNqeGJyNnVhYzA3MWwzeXBlaHlzc3ZleG4ifQ.TwvVQCK3WtQFEVKGdEp1kg';
+
     const map = new mapboxgl.Map({
-      container: 'map',
-      style: 'mapbox://styles/mapbox/streets-v11'
+      style: 'mapbox://styles/mapbox/light-v10',
+      center: [this.lng, this.lat],
+      zoom: 15.5,
+      pitch: 45,
+      bearing: -17.6,
+      container: 'map'
+    });
+
+    map.on('load', () => {
+
+      map.resize();
+
+      new mapboxgl.Marker().setLngLat([this.lng, this.lat]).addTo(map);
+
+      // Insert the layer beneath any symbol layer.
+      const layers = map.getStyle().layers;
+
+      let labelLayerId;
+
+      for (let i = 0; i < layers.length; i++) {
+
+        if (layers[i].type === 'symbol' && layers[i].layout['text-field']) {
+
+          labelLayerId = layers[i].id;
+          break;
+
+        }
+
+      }
+
+      map.addLayer({
+        'id': '3d-buildings',
+        'source': 'composite',
+        'source-layer': 'building',
+        'filter': [
+          '==',
+          'extrude',
+          'true'
+        ],
+        'type': 'fill-extrusion',
+        'minzoom': 15,
+        'paint': {
+          'fill-extrusion-color': '#aaa',
+          'fill-extrusion-height': [
+            'interpolate',
+            [ 'linear' ],
+            [ 'zoom' ],
+            15,
+            0,
+            15.05,
+            [ 'get', 'height' ]
+          ],
+          'fill-extrusion-base': [
+            'interpolate',
+            [ 'linear' ],
+            [ 'zoom' ],
+            15,
+            0,
+            15.05,
+            [ 'get', 'min_height' ]
+          ],
+          'fill-extrusion-opacity': .6
+        }
+      }, labelLayerId);
     });
 
   }
