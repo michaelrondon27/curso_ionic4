71910661f27c8d965df05efc86fecccc9514f4a9
Michael Rondon <m>
2019-07-16 18:15:53 +0200

    195. Levantar la base de datos MongoDB y conectar nuestra aplicacion

--- 08-fotosgram-server/dist/index.js
+++ 08-fotosgram-server/dist/index.js
@@ -5,9 +5,19 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const server_1 = __importDefault(require("./classes/server"));
 const usuario_1 = __importDefault(require("./routes/usuario"));
+const mongoose_1 = __importDefault(require("mongoose"));
 const server = new server_1.default();
 // Rutas de mi app
 server.app.use('/user', usuario_1.default);
+// Conectar DB
+mongoose_1.default.connect('mongodb://localhost:27017/fotosgram', {
+    useNewUrlParser: true,
+    useCreateIndex: true
+}, (err) => {
+    if (err)
+        throw err;
+    console.log('Base de datos ONLINE');
+});
 // Levantar express
 server.start(() => {
     console.log(`servidor corriend en puerto ${server.port}`);
--- 08-fotosgram-server/index.ts
+++ 08-fotosgram-server/index.ts
@@ -1,10 +1,25 @@
 import Server from "./classes/server";
 import userRoutes from "./routes/usuario";
+import mongoose from 'mongoose';
 
 const server = new Server();
 
 // Rutas de mi app
 server.app.use('/user', userRoutes);
+
+// Conectar DB
+mongoose.connect(
+    'mongodb://localhost:27017/fotosgram',
+    {
+        useNewUrlParser: true,
+        useCreateIndex: true
+    },
+    (err) => {
+        if ( err ) throw err;
+
+        console.log('Base de datos ONLINE');
+    }
+);
 
 // Levantar express
 server.start( () => {
