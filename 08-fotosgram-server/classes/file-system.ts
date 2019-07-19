d0efe73875e0e048bad254a8d3583f4a736188fb
Michael Rondon <m>
2019-07-19 23:31:31 +0200

    214. Generar un nombre unico a la imagen

--- 08-fotosgram-server/classes/file-system.ts
+++ 08-fotosgram-server/classes/file-system.ts
@@ -1,6 +1,7 @@
 import { FileUpload } from "../interfaces/file-upload";
 import path from 'path';
 import fs from 'fs';
+import uniqid from 'uniqid';
 
 export default class FileSystem {
 
@@ -8,7 +9,23 @@ export default class FileSystem {
 
     guardarImagenTemporal( file: FileUpload, userId: string ) {
 
+        // Crear carpetas
         const path = this.crearCarpetaUsuario( userId );
+
+        // Nombre archivo
+        const nombreArchivo = this.generarNombreUnico( file.name );
+
+    }
+
+    private generarNombreUnico( nombreOriginal: string ) {
+
+        const nombreArr = nombreOriginal.split('.');
+
+        const extension = nombreArr[ nombreArr.length - 1 ];
+
+        const idUnico = uniqid();
+
+        return `${ idUnico }.${ extension }`;
 
     }
 
--- 08-fotosgram-server/dist/classes/file-system.js
+++ 08-fotosgram-server/dist/classes/file-system.js
@@ -5,10 +5,20 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const path_1 = __importDefault(require("path"));
 const fs_1 = __importDefault(require("fs"));
+const uniqid_1 = __importDefault(require("uniqid"));
 class FileSystem {
     constructor() { }
     guardarImagenTemporal(file, userId) {
+        // Crear carpetas
         const path = this.crearCarpetaUsuario(userId);
+        // Nombre archivo
+        const nombreArchivo = this.generarNombreUnico(file.name);
+    }
+    generarNombreUnico(nombreOriginal) {
+        const nombreArr = nombreOriginal.split('.');
+        const extension = nombreArr[nombreArr.length - 1];
+        const idUnico = uniqid_1.default();
+        return `${idUnico}.${extension}`;
     }
     crearCarpetaUsuario(userId) {
         const pathUser = path_1.default.resolve(__dirname, '../uploads/', userId);
