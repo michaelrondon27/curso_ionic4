c2b9219a580c6c2e5bd1f9a40c0cfd359aba842f
Michael Rondon <m>
2019-09-02 19:42:15 +0200

    227. Interfaces necesarias para el tipado de nuestros datos

--- 09-fotosgram/src/app/services/posts.service.ts
+++ 09-fotosgram/src/app/services/posts.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from '../../environments/environment';
+import { RespuestaPosts } from '../interfaces/interfaces';
 
 const URL = environment.url;
 
@@ -19,7 +20,7 @@ export class PostsService {
 
     this.paginaPosts ++;
 
-    return this.http.get(`${ URL }/posts/?pagina=${ this.paginaPosts }`);
+    return this.http.get<RespuestaPosts>(`${ URL }/posts/?pagina=${ this.paginaPosts }`);
 
   }
 
