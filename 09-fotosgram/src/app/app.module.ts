20827d042b2146514332538c4f6f83ce8d170306
Michael Rondon <m>
2019-09-02 19:24:27 +0200

    224. Obtener el listado de nuestros posts

--- 09-fotosgram/src/app/app.module.ts
+++ 09-fotosgram/src/app/app.module.ts
@@ -6,13 +6,20 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
 
+import { HttpClientModule } from '@angular/common/http';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [
+    AppRoutingModule,
+    BrowserModule,
+    HttpClientModule,
+    IonicModule.forRoot()
+  ],
   providers: [
     StatusBar,
     SplashScreen,
--- 09-fotosgram/src/app/pages/tab1/tab1.page.ts
+++ 09-fotosgram/src/app/pages/tab1/tab1.page.ts
@@ -1,12 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PostsService } from '../../services/posts.service';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page {
+export class Tab1Page implements OnInit {
 
-  constructor() {}
+  constructor(
+    private postsService: PostsService
+  ) {}
+
+  ngOnInit() {
+
+    this.postsService.getPosts().subscribe( resp => {
+
+      console.log(resp);
+
+    });
+
+  }
 
 }
