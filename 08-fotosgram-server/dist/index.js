0acfc5d9fff2a2f3db49f3e61e9a9b69081a4b05
Michael Rondon <m>
2019-09-02 19:32:09 +0200

    225. Backend - CORS - Resolver el problema de peticiones CROSS Domain

--- 08-fotosgram-server/dist/index.js
+++ 08-fotosgram-server/dist/index.js
@@ -5,6 +5,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const server_1 = __importDefault(require("./classes/server"));
 const mongoose_1 = __importDefault(require("mongoose"));
+const cors_1 = __importDefault(require("cors"));
 const body_parser_1 = __importDefault(require("body-parser"));
 const express_fileupload_1 = __importDefault(require("express-fileupload"));
 const usuario_1 = __importDefault(require("./routes/usuario"));
@@ -15,6 +16,8 @@ server.app.use(body_parser_1.default.urlencoded({ extended: true }));
 server.app.use(body_parser_1.default.json());
 // FileUpload
 server.app.use(express_fileupload_1.default());
+// Configurar CORS
+server.app.use(cors_1.default({ origin: true, credentials: true }));
 // Rutas de mi app
 server.app.use('/user', usuario_1.default);
 server.app.use('/posts', post_1.default);
--- 08-fotosgram-server/index.ts
+++ 08-fotosgram-server/index.ts
@@ -1,6 +1,8 @@
 import Server from "./classes/server";
 import mongoose from 'mongoose';
 
+import cors from 'cors';
+
 import bodyParser from 'body-parser';
 import fileUpload from 'express-fileupload';
 
@@ -15,6 +17,9 @@ server.app.use( bodyParser.json() );
 
 // FileUpload
 server.app.use( fileUpload() );
+
+// Configurar CORS
+server.app.use( cors({ origin: true, credentials: true }) );
 
 // Rutas de mi app
 server.app.use('/user', userRoutes);
