90e498b11cbb74e5ce9b50f185c11d2e8c449056
Michael Rondon <m>
2019-09-10 20:16:52 +0200

    251. IOS - DomSanitizer para las imagenes

--- 09-fotosgram/src/app/pages/tab2/tab2.module.ts
+++ 09-fotosgram/src/app/pages/tab2/tab2.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Tab2Page } from './tab2.page';
+import { PipesModule } from '../../pipes/pipes.module';
 
 @NgModule({
   imports: [
     IonicModule,
     CommonModule,
     FormsModule,
+    PipesModule,
     RouterModule.forChild([{ path: '', component: Tab2Page }])
   ],
   declarations: [Tab2Page]
